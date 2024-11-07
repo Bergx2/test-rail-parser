0cd9f078a189c0aa3bd8f4d75fd5939310a7f593
gulidovbergx2 <sergey.gulidov@bergx2.de>
2024-11-07 17:23:17 +0200

    SWWEB-2186: add logic removing test cases and suites

--- server/helpers/baseHelper.js
+++ server/helpers/baseHelper.js
@@ -1,9 +1,13 @@
 const reduce = require('lodash/reduce');
 const assign = require('lodash/assign');
+const isEqual = require('lodash/isEqual');
 
 const formateObjectsByKey = (objects, key) =>
   reduce(objects, (acc, object) => assign(acc, { [object[key]]: object }), {});
 
+const isTrue = value => isEqual(value, 'true');
+
 module.exports = {
   formateObjectsByKey,
+  isTrue,
 };
